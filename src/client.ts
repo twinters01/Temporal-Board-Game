8ed0b28556f1da93e322bf72882c7e3d7858ee7d
twinters01 <twinters01@hotmail.com>
2025-01-27 22:57:19 -0500

    signals reading

--- src/client.ts
+++ src/client.ts
@@ -1,14 +1,14 @@
 // @@@SNIPSTART money-transfer-project-template-ts-start-workflow
 import { Connection, WorkflowClient } from '@temporalio/client';
 import { uuid4 } from '@temporalio/workflow';
-import { helloWorld } from './workflows';
+import { game } from './workflows';
 
 async function run() {
     console.log("run")
   const connection = await Connection.connect();
   const client = new WorkflowClient({ connection });
 
-    const handle = await client.start(helloWorld, {
+    const handle = await client.start(game, {
         taskQueue: 'snippets',
         workflowId: uuid4()
     })
--- src/workflows.ts
+++ src/workflows.ts
@@ -1,6 +1,30 @@
+import { condition, defineSignal, setHandler } from '@temporalio/workflow';
 import {greet} from './activities';
+
+const playerRollSignal = defineSignal<[number, number]>('playerRoll')
 
 export async function helloWorld()
 {
-    return {greeting: await greet()};
+    return { greeting: await greet() };
+}
+
+export async function game()
+{
+    var goal = 30;
+    var players = [0, 0];
+    var currentPlayer = 0;
+    setHandler(playerRollSignal, (player) => {
+        if(player == currentPlayer)
+        {
+            players[player] += 30;
+            currentPlayer = currentPlayer == 0 ? 1 : 0;
+        }
+    });
+
+    await condition(() => players[0] >= goal || players[1] >= goal);
+
+    if(players[0] >= goal)
+        return 0;
+    else if(players[1] >= goal)
+        return 1;
 }
